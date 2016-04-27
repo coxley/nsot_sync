602a4caf792856ad39668c859ce5805b4aab2af6
jathanism <jathan@gmail.com>
2016-04-26 21:57:21 -0700

    Fix #24: Updated to work with NSoT API version 1.0 and pynsot 0.22.2
    - Removed unused dependency: IPy==0.83
    - Removed redundant depdendency (provided by pynsot): Click==6.2
    - Removed download_url from setup.py; added shebang; made executable
    - Removed tests/__pycache__ from version control

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
-VERSION = '0.4.8'
+VERSION = '0.5'
 
 setup(
     name='nsot_sync',
@@ -9,17 +11,14 @@ setup(
     author='Codey Oxley',
     author_email='codey.a.oxley+os@gmail.com',
     url='https://github.com/coxley/nsot_sync',
-    download_url='https://github.com/coxley/nsot_sync/tarball/%s' % VERSION,
     keywords=['networking', 'ipam', 'nsot', 'cmdb', 'sync', 'orion',
               'solarwinds', 'infoblox', 'ip', 'address'],
     classifiers=[],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click==6.2',
-        'pynsot==0.19.1',
+        'pynsot==0.22.2',
         'netifaces==0.10.4',
-        'IPy==0.83',
         'coloredlogs==5.0',
     ],
     extras_require={
